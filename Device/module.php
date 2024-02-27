97893afae6145f8b5229cfa461fe2e56b0473d44
Andreas Seemueller <andreas.seemueller@gmail.com>
2024-02-27 18:16:33 +0100

    sdf

--- Device/module.php
+++ Device/module.php
@@ -213,7 +213,11 @@ class ProjectorMQTTDevice extends IPSModule
             case "IntensityRotation":
                 $this->MqttSet('dps/101/command', $value * 10);
                 SetValue($this->GetIDForIdent($ident), $value);
-                break;       
+                break;
+            case "ColorNebula":
+                $colorTuya = $this->decToTuya($value);
+                $this->SendDebug('COLOR CONVERSION',"dec: $value hex: $colorTuya", 0);
+                break;     
 			default:
 				$this->LogMessage("Invalid Ident: $ident", KL_ERROR);
 		}
@@ -230,5 +234,65 @@ class ProjectorMQTTDevice extends IPSModule
         $this->SendDebug('MQTT SEND Topic', $Data['Topic'], 0);
         $this->SendDebug('MQTT SEND Payload', $Data['Payload'], 0);
         $this->SendDataToParent(json_encode($Data));
+    }
+
+    protected function RGB_TO_HSV($R, $G, $B)  // RGB Values:Number 0-255 
+    {                                 // HSV Results:Number 0-1 
+       $HSL = array(); 
+
+       $var_R = ($R / 255); 
+       $var_G = ($G / 255); 
+       $var_B = ($B / 255); 
+
+       $var_Min = min($var_R, $var_G, $var_B); 
+       $var_Max = max($var_R, $var_G, $var_B); 
+       $del_Max = $var_Max - $var_Min; 
+
+       $V = $var_Max; 
+
+       if ($del_Max == 0) 
+       { 
+          $H = 0; 
+          $S = 0; 
+       } 
+       else 
+       { 
+          $S = $del_Max / $var_Max; 
+
+          $del_R = ( ( ( $var_Max - $var_R ) / 6 ) + ( $del_Max / 2 ) ) / $del_Max; 
+          $del_G = ( ( ( $var_Max - $var_G ) / 6 ) + ( $del_Max / 2 ) ) / $del_Max; 
+          $del_B = ( ( ( $var_Max - $var_B ) / 6 ) + ( $del_Max / 2 ) ) / $del_Max; 
+
+          if      ($var_R == $var_Max) $H = $del_B - $del_G; 
+          else if ($var_G == $var_Max) $H = ( 1 / 3 ) + $del_R - $del_B; 
+          else if ($var_B == $var_Max) $H = ( 2 / 3 ) + $del_G - $del_R; 
+
+          if ($H<0) $H++; 
+          if ($H>1) $H--; 
+       } 
+
+       $HSL['H'] = $H; 
+       $HSL['S'] = $S; 
+       $HSL['V'] = $V; 
+
+       return $HSL; 
+    } 
+
+    // From Symcon decimal value to tuya hex encoding
+    protected function decToTuya ($decColor) {
+
+	    $decRed = floor($decColor/65536); 
+	    $decBlue  = floor(($decColor-($decRed*65536))/256);
+	    $decGreen = $decColor-($decBlue*256)-($decRed*65536);
+
+        $decHSV = $this->RGB_TO_HSV($decRed, $decBlue, $decGreen);
+
+        $hexH = sprintf('%04x', $decHSV['H']);
+        $hexS = sprintf('%04x', $decHSV['S']);
+        $hexV = sprintf('%04x', $decHSV['V']);
+
+        $colorTuya = $hexH . $hexS . $hexV;
+
+        return $colorTuya;
     }
 }
