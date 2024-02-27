dd093699b16eb635ca2c41ac1d96c8f301092566
Andreas Seemueller <andreas.seemueller@gmail.com>
2024-02-27 21:23:59 +0100

    test

--- Device/module.php
+++ Device/module.php
@@ -241,55 +241,6 @@ class ProjectorMQTTDevice extends IPSModule
         $this->SendDataToParent(json_encode($Data));
     }
 
-    protected function RGB_TO_HSV($R, $G, $B)  // RGB Values:Number 0-255 
-    {                                 // HSV Results:Number 0-1 
-       $HSL = array(); 
-
-       $var_R = ($R / 255); 
-       $var_G = ($G / 255); 
-       $var_B = ($B / 255); 
-
-       $var_Min = min($var_R, $var_G, $var_B); 
-       $var_Max = max($var_R, $var_G, $var_B); 
-       $del_Max = $var_Max - $var_Min; 
-
-       $V = $var_Max; 
-
-       if ($del_Max == 0) 
-       { 
-          $H = 0; 
-          $S = 0; 
-       } 
-       else 
-       { 
-          $S = $del_Max / $var_Max; 
-
-          $del_R = ( ( ( $var_Max - $var_R ) / 6 ) + ( $del_Max / 2 ) ) / $del_Max; 
-          $del_G = ( ( ( $var_Max - $var_G ) / 6 ) + ( $del_Max / 2 ) ) / $del_Max; 
-          $del_B = ( ( ( $var_Max - $var_B ) / 6 ) + ( $del_Max / 2 ) ) / $del_Max; 
-
-          if      ($var_R == $var_Max) $H = $del_B - $del_G; 
-          else if ($var_G == $var_Max) $H = ( 1 / 3 ) + $del_R - $del_B; 
-          else if ($var_B == $var_Max) $H = ( 2 / 3 ) + $del_G - $del_R; 
-
-          if ($H<0) $H++; 
-          if ($H>1) $H--; 
-       } 
-
-       // Adjusting to required values for tuya
-       // $H = $H * 100;
-       $S = $S * 100;
-       $V = $V * 100;
-
-       $HSL['H'] = $H; 
-       $HSL['S'] = $S; 
-       $HSL['V'] = $V; 
-
-       $this->SendDebug('COLOR CONVERSION RGB->HSV',"R: $R G: $G B: $B / H: $H S: $S V: $V", 0);
-
-       return $HSL; 
-    } 
-
     // found here: https://stackoverflow.com/a/13887939
     protected function RGBtoHSV($R, $G, $B)    // RGB values:    0-255, 0-255, 0-255
     {                                // HSV values:    0-360, 0-100, 0-100
